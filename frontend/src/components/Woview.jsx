ae8308b6d9cd11126029d2af457ffe2d031ebf40
giwoong kim <pooh_hoop@naver.com>
2021-10-21 17:46:30 +0900

    1021_view_add

--- frontend/src/components/Woview.jsx
+++ frontend/src/components/Woview.jsx
@@ -1,96 +1,181 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../App.css";
 
 class Woview extends Component {
-    render() {
-        return (
-            
-              
-        <div class="card">       
-            <div class="card-header">
-              <div class="post">사용자 정보</div>
+  render() {
+    return (
+      <div class="row">
+        <div class="col-12">
+          <div class="card card-primary card-outline card-outline-tabs">
+            <div class="card-header p-0 border-bottom-0">
+              <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
+                <li class="nav-item">
+                  <a
+                    class="nav-link active"
+                    id="custom-tabs-one-home-tab"
+                    data-toggle="pill"
+                    href="#custom-tabs-one-home"
+                    role="tab"
+                    aria-controls="custom-tabs-one-home"
+                    aria-selected="true"
+                  >
+                    목록
+                  </a>
+                </li>
+              </ul>
             </div>
-         <form>
-         
-          <div class="card-body">
-          
-            <div class="row">
-              <div class="col-sm-2">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">변경 번호</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="변경 번호"/>
+            <div class="card-body">
+              <div class="card">
+                <div class="card-header">
+                  <div class="post">현재 할당 내역</div>
+                </div>
+
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">변경</label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          placeholder="변경 번호"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">상태</label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          placeholder="요약"
+                        />
+                      </div>
+                    </div>
                   </div>
-                  
+                </div>
               </div>
-              <div class="col-sm-3">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">요약</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="요약"/>
+
+              <div class="card">
+                <form>
+                  <div class="card-header">
+                    <div class="post">사용자 정보</div>
                   </div>
-                  
+
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-sm-2">
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">작성자</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            placeholder="변경 번호"
+                          />
+                        </div>
+                      </div>
+                      <div class="col-sm-3">
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">휴대폰 번호</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            placeholder="요약"
+                          />
+                        </div>
+                      </div>
+                      <div class="col-sm-2">
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">요청자</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            placeholder="작성자"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card-header">
+                    <div class="post">상세 정보</div>
+                  </div>
+
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-sm-3">
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">변경 개요</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            placeholder="변경 번호"
+                          />
+                        </div>
+
+                        <div class="form-group">
+                          <label>구성품목</label>
+                          <select
+                            class="form-control select2"
+                            style={{ width: "70%" }}
+                          >
+                            <option selected="selected">Alabama</option>
+                            <option>Alaska</option>
+                            <option>California</option>
+                            <option>Delaware</option>
+                            <option>Tennessee</option>
+                            <option>Texas</option>
+                            <option>Washington</option>
+                          </select>
+                        </div>
+                      </div>
+
+                      <div class="col-sm-3">
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">분류</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            placeholder="분류"
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">등급</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            placeholder="등급"
+                          />
+                        </div>
+
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">변경 유형</label>
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            placeholder="변경 유형"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
               </div>
-              <div class="col-sm-2">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">작성자</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="작성자"/>
-                  </div>      
-              </div>        
             </div>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label>Minimal</label>
-                  <select class="form-control select2" style={{width: '100%'}}>
-                    <option selected="selected">Alabama</option>
-                    <option>Alaska</option>
-                    <option>California</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
-                  </select>
-                </div>
-               
-              </div>
-            
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label>Multiple</label>
-                  <select class="select2" multiple="multiple" data-placeholder="Select a State" style={{width: '100%'}}>
-                    <option>Alabama</option>
-                    <option>Alaska</option>
-                    <option>California</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
-                  </select>
-                </div>
-                
-                
-              </div>
-            </div>    
           </div>
-         </form>
-         <div class="card-header">
-            <div class="post">상세 정보</div>
-         </div>
-               
-          <div class="card-body">
-            <div class="row">
-              <div class="col-sm-2">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">변경 번호</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="변경 번호"/>
-                  </div>
-                  
-              </div>
-              </div>
-              </div>
         </div>
-    
-           
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default Woview;
